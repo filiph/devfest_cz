798885282b2e03b2e7cb09d688160e7de0846f25
Filip Hracek <filip.hracek@gmail.com>
2018-10-24 16:27:51 +0200

    Don’t recompute fibonacci on every frame

--- lib/pages/compute.dart
+++ lib/pages/compute.dart
@@ -10,6 +10,7 @@ class ComputeSlide extends StatefulWidget {
 
 class ComputeSlideState extends State<ComputeSlide> {
   int _n = 1;
+  double _value = 1.0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,18 +25,37 @@ class ComputeSlideState extends State<ComputeSlide> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Slider(
-              min: 1.0,
-              max: 40.0,
-              value: _n.toDouble(),
-              onChanged: (value) {
-                setState(() {
-                  _n = value.toInt();
-                });
+            StatefulBuilder(
+              builder: (context, innerSetState) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    SizedBox(height: 30.0),
+                    Text('fib(${_value.round()}) = '),
+                    Slider(
+                      min: 1.0,
+                      max: 40.0,
+                      value: _value,
+                      onChanged: (value) {
+                        innerSetState(() {
+                          _value = value;
+                        });
+                      },
+                      onChangeEnd: (value) {
+                        setState(() {
+                          _n = value.toInt();
+                        });
+                      },
+                    ),
+                  ],
+                );
               },
             ),
-            Text('fib($_n) = '),
-            FibonacciResult(n: _n),
+            ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 50.0),
+              child: FibonacciResult(n: _n, key: ValueKey(_n)),
+            ),
           ],
         ),
       ),
@@ -57,15 +77,14 @@ class FibonacciResult extends StatelessWidget {
       future: compute(fibonacci, n),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        }
-        if (!snapshot.hasData) {
-          return CircularProgressIndicator();
+          return SizedBox(
+            width: 20.0,
+            height: 20.0,
+            child: CircularProgressIndicator(),
+          );
         }
         return Container(
-          child: Center(
-            child: Text('${snapshot.data}'),
-          ),
+          child: Text('${snapshot.data}'),
         );
       },
     );
